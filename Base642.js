df7974045e796f47ebbc4dc680dd2982fb60ec4d
vendravex <vendravex@gmail.com>
2021-04-14 20:59:56 +0700

    Update and rename Base641.js to Base642.js

--- Base642.js
+++ Base642.js
@@ -20,7 +20,7 @@ j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 xxx = setting.CatTag[Math.floor(Math.random() * setting.CatTag.length)];
-a_to_ve[i].href= setting.domainSafelink +xxx+ "?site=" + encryptedUrl;a_to_ve[i].rel="nofollow";a_to_ve[i].target="_blank";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+a_to_ve[i].href= setting.domainSafelink + xxx + "?site=" + encryptedUrl;a_to_ve[i].rel="nofollow";a_to_ve[i].target="_blank";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
